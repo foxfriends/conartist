b92bff3fe4bda04aa1c6cd651a0bad412b1ac39a
Cameron Eldridge <cameldridge+git@gmail.com>
2017-07-13 18:20:52 -0400

    Fix history page

--- app/conartist/app/con-view/history/index.js
+++ app/conartist/app/con-view/history/index.js
@@ -9,22 +9,40 @@ import type { Record } from '../../conartist.types';
 
 import { views, text } from '../../styles'
 
+const unique = <T>(v: T, i: number, arr: T[]): boolean => arr.indexOf(v) === i;
+
 export default class History extends Component {
   static navigationOptions = {
     title: 'History',
     tabBarIcon: <Icon name='history' size={24} color='white'/>,
   };
 
+  product(id: number): Product {
+    return this.props.screenProps.data.products.find(_ => _.id === id);
+  }
+
+  type(record: Record): ProductType {
+    const products = record.products.filter(unique).map(id => this.product(id));
+    const typeIds = products.map(_ => _.type).filter(unique);
+    if(typeIds.length === 1) {
+      return this.props.screenProps.data.types.find(_ => _.id === typeIds[0]);
+    } else {
+      return { color: 0x000000, name: '?', discontinued: false, id: -1 };
+    }
+  }
+
   renderListItem(item: Record) {
-    // TODO: make this meaningful
     const products = item.products.reduce((o, p) => (o[p] = o[p] ? o[p] + 1 : 1, o), {});
-    const productStr = Object.keys(products).map(key =>
-      products[key] === 1 ? key : `${key} (${products[key]})`).join(', ');
+    const productStr = Object.keys(products).map(key => {
+      const name = this.product(+key).name;
+      return products[key] === 1 ? name : `${name} (${products[key]})`;
+    }).join(', ');
+    const type = this.type(item);
     const contents = (
       <View style={[views.listItem, views.paper, views.padded, { flexDirection: 'row', alignItems: 'center' }]}>
-        <View style={[views.circle, { backgroundColor: this.props.screenProps.data.colors[item.type] }]}>
+        <View style={[views.circle, { backgroundColor: `#${type.color.toString(16)}` }]}>
           <View style={[views.flex, views.vMiddle, views.hMiddle]}>
-            <Text style={[text.icon, { color: 'white' }]}>{ item.type[0] }</Text>
+            <Text style={[text.icon, { color: 'white' }]}>{ type.name[0] }</Text>
           </View>
         </View>
         <View style={[views.hPadded]}>

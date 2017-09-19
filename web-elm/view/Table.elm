2f5b64ea1b44e015babe80fd1d9a0a7971f61ba6
Cameron Eldridge <cameldridge+git@gmail.com>
2017-09-19 13:49:32 -0400

    Add some table styles

--- web-elm/view/Table.elm
+++ web-elm/view/Table.elm
@@ -1,19 +1,16 @@
 module Table exposing (table)
 import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, style)
 
 table : List String -> (a -> List (Html msg)) -> List a -> Html msg
-table columnHeaders rowOfColumns data =
-  div [ class "table" ]
-    ( div [ class "table__row--header" ] (List.map headerColumn columnHeaders)
-   :: List.map (rowOfColumns >> row) data )
+table columnHeaders columns data =
+  div
+    [ class "table"
+    , style [ ("grid-template-columns", String.repeat 4 "1fr ") ] ]
+    ((List.map headerColumn columnHeaders) ++ List.map column (List.concatMap columns data))
 
 headerColumn : String -> Html msg
-headerColumn title = div [ class "table__column--header" ] [ text title ]
-
-row : List (Html msg) -> Html msg
-row columns =
-  div [ class "table__row" ] <| List.map column columns
+headerColumn title = div [ class "table__cell--header" ] [ text title ]
 
 column : Html msg -> Html msg
-column data = div [ class "table__column" ] [ data ]
+column data = div [ class "table__cell" ] [ data ]

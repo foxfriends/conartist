57e6a45d53c0cfc105e3ec2953f80d2d4b527e1e
Cameron Eldridge <cameldridge+git@gmail.com>
2017-10-15 18:18:28 -0400

    Rename id fields to be all called id

--- server-rs/src/graphql/product.rs
+++ server-rs/src/graphql/product.rs
@@ -5,7 +5,7 @@ pub use database::Product;
 graphql_object!(Product: Database |&self| {
     description: "Holds information about a product"
 
-    field product_id() -> i32 { self.product_id }
+    field id() -> i32 { self.product_id }
     field type_id() -> i32 { self.type_id }
     field name() -> &String { &self.name }
     field discontinued() -> bool { self.discontinued }
--- server-rs/src/graphql/product_type.rs
+++ server-rs/src/graphql/product_type.rs
@@ -5,7 +5,7 @@ pub use database::ProductType;
 graphql_object!(ProductType: Database |&self| {
     description: "Holds information about a product type"
 
-    field type_id() -> i32 { self.type_id }
+    field id() -> i32 { self.type_id }
     field name() -> &String { &self.name }
     field color() -> i32 { self.color }
     field discontinued() -> bool { self.discontinued }

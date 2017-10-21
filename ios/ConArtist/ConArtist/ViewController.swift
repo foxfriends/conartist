02b8dbeb995c151f518ea431927e146b25dfbc65
Yeva Yu <yevayu@outlook.com>
2017-10-21 12:29:32 -0400

    initial ios commit

---
+++ ios/ConArtist/ConArtist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConArtist
+//
+//  Created by Cameron Eldridge on 2017-10-21.
+//  Copyright © 2017 Cameron Eldridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

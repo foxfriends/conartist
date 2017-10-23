a32db4f1743d839c44848b3953e2ad052292568f
Cameron Eldridge <cameldridge+git@gmail.com>
2017-10-23 13:38:06 -0400

    Clicking the products adds them to the selected list

--- android/app/src/main/java/com/cameldridge/conartist/CreateRecordActivity.kt
+++ android/app/src/main/java/com/cameldridge/conartist/CreateRecordActivity.kt
@@ -1,22 +1,30 @@
 package com.cameldridge.conartist
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.MenuItem
 import com.cameldridge.conartist.schema.Price
+import com.cameldridge.conartist.schema.PricePair
 import com.cameldridge.conartist.schema.Product
 import com.cameldridge.conartist.schema.ProductType
 
 import kotlinx.android.synthetic.main.activity_create_record.*
+import kotlinx.android.synthetic.main.product_chip.view.*
 import kotlin.properties.Delegates
 
 class CreateRecordActivity : AppCompatActivity() {
     private var productType: ProductType by Delegates.notNull()
     private var products: ArrayList<Product> by Delegates.notNull()
     private var prices: ArrayList<Price> by Delegates.notNull()
+    private var defaultPrice: Price by Delegates.notNull()
+    private var selected: ArrayList<Product> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +33,78 @@ class CreateRecordActivity : AppCompatActivity() {
         productType = intent.getParcelableExtra(PRODUCT_TYPE)
         products = intent.getParcelableArrayListExtra(PRODUCTS)
         prices = intent.getParcelableArrayListExtra(PRICES)
+        defaultPrice = prices
+            .find { it.productId == null }
+            ?: Price(productType.id, null, arrayListOf(PricePair(1, 0.0)))
 
         supportActionBar?.title = productType.name
         val adapter = ProductRowListAdapter(this)
         adapter.addAll(products)
         productsList.adapter = adapter
-        productsList.setOnItemClickListener { parent, _, position, _ -> println((parent.getItemAtPosition(position) as Product).name) }
+        productsList.setOnItemClickListener { parent, _, position, _ ->
+            val product = parent.getItemAtPosition(position) as Product
+            addProduct(product)
+        }
 
         // TODO: make this a save button that sends the GraphQL mutation or saves locally
         //       and sends it when there is internet connection
-        fab.setOnClickListener { view -> finish() }
+        saveButton.setOnClickListener { view -> finish() }
+        updateSaveButtonState()
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun addProduct(product: Product) {
+        selected.add(product)
+        val chip = LayoutInflater.from(this).inflate(R.layout.product_chip, chips, false)
+        chip.productName.text = product.name
+        chip.setOnClickListener { removeProduct(chips.indexOfChild(it)) }
+        chips.addView(chip)
+        priceField.setText("%.2f".format(calculatePrice()))
+        updateSaveButtonState()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun removeProduct(index: Int) {
+        selected.removeAt(index)
+        chips.removeViewAt(index)
+        priceField.setText("%.2f".format(calculatePrice()))
+        updateSaveButtonState()
+    }
+
+    private fun updateSaveButtonState() {
+        if(selected.size == 0) {
+            saveButton.isEnabled = false
+            saveButton.background.colorFilter = PorterDuffColorFilter(0xFF999999.toInt(), PorterDuff.Mode.SRC_IN)
+        } else {
+            saveButton.isEnabled = true
+            saveButton.background.colorFilter = null
+        }
+    }
+
+    private fun calculatePrice(): Double =
+        selected
+            .fold(emptyMap()) { map: Map<Price, Int>, product ->
+                val price = prices.find { it.productId == product.id } ?: defaultPrice
+                map.plus(price to (map[price] ?: 0) + 1)
+            }
+            .asIterable()
+            .fold(0.0) { total, entry ->
+                val price = entry.key
+                var qty = entry.value
+                var money = 0.0
+                while(qty > 0) {
+                    val pair = price.prices.fold(PricePair(1, 0.0)) { best, pair ->
+                        if(pair.quantity >= best.quantity && pair.quantity <= qty) {
+                            pair
+                        } else {
+                            best
+                        }
+                    }
+                    qty -= pair.quantity
+                    money += pair.price
+                }
+                total + money
+            }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == android.R.id.home) {

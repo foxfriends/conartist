ad3afc082fe04d55f41c73c9c2651451d862e97e
Cameron Eldridge <cameldridge+git@gmail.com>
2018-03-05 21:10:41 -0500

    Fix a few things, write half a screen idk anymore

---
+++ ios/ConArtist/Scenes/Convention Details/ConventionUserInfoListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConventionUserInfoListViewController.swift
+//  ConArtist
+//
+//  Created by Cameron Eldridge on 2018-03-05.
+//  Copyright © 2018 Cameron Eldridge. All rights reserved.
+//
+
+import Foundation
---
+++ ios/ConArtist/Scenes/Convention Details/ConventionUserInfoTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ConventionUserInfoTableViewCell.swift
+//  ConArtist
+//
+//  Created by Cameron Eldridge on 2018-03-05.
+//  Copyright © 2018 Cameron Eldridge. All rights reserved.
+//
+
+import Foundation

ec274b8b9140288740da249a88c4e1a12f4fa99c
Cameron Eldridge <cameldridge+git@gmail.com>
2018-04-25 19:42:33 -0400

    Rename file

--- web/src/common/vote/index.jsx
+++ web/src/common/vote/index.jsx
@@ -22,8 +22,8 @@ export function Vote({ className, vote, upvotes, downvotes, onChange }: Props) {
   return (
     <div className={`${S.container} ${className || ''}`}>
       <div className={S.thumbs}>
-        <IconButton quiet priority='secondary' title='thumb_down' className={`${S.thumbDown} ${voteClass}`} action={() => onChange && onChange(vote === -1 ? 0 : -1)} />
-        <IconButton quiet priority='secondary' title='thumb_up' className={`${S.thumbUp} ${voteClass}`} action={() => onChange && onChange(vote === 1 ? 0 : 1)}/>
+        <IconButton quiet priority='secondary' title='thumb_down' className={`${S.thumbDown} ${voteClass}`} action={() => onChange && onChange(vote === -1 ? 1 : -1)} />
+        <IconButton quiet priority='secondary' title='thumb_up' className={`${S.thumbUp} ${voteClass}`} action={() => onChange && onChange(vote === 1 ? -1 : 1)}/>
       </div>
       <span className={S.score}>{upvotes - downvotes}</span>
     </div>

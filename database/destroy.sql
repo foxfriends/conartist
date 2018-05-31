56940107640e60b9aa5ec5dd66e05b67a3609d87
Cameron Eldridge <cameldridge+git@gmail.com>
2018-05-31 19:07:24 -0400

    Create docker stuff :o

--- database/destroy.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE conartist;
-DROP ROLE conartist_admin;
-DROP OWNED BY conartist_app;
-DROP ROLE conartist_app;
--- database/profit-by-type.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  SUM(price),
-  t.name
-FROM
-  (SELECT price, products[1] AS product_id FROM Records) AS r
-  INNER JOIN Products p ON p.product_id = r.product_id
-  INNER JOIN ProductTypes t ON p.type_id = t.type_id
-GROUP BY t.name;

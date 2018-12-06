bd0bb055c4aab796f8a38deb8ed47df09c7f3997
Cameron Eldridge <cameldridge+git@gmail.com>
2018-12-06 02:25:44 -0500

    Use some small caps

--- web/src/content/settings/help.js
+++ web/src/content/settings/help.js
@@ -6,6 +6,7 @@ import { Row } from '../../common/table/row'
 import { Link } from '../../common/link'
 import { l } from '../../localization'
 import * as navigate from '../../update/navigate'
+import S from './row.css'
 
 export type Props = {}
 
@@ -13,9 +14,9 @@ export function Help() {
   return (
     <BasicCard title={l`Help`}>
       <Table>
-        <Row title={l`Report a bug/Request a feature`} detail={<Link onClick={navigate.suggestions}>{l`View`}</Link>} />
-        <Row title={l`Privacy Policy`} detail={<Link onClick={navigate.privacy}>{l`View`}</Link>} />
-        <Row title={l`Terms of Service`} detail={<Link onClick={navigate.terms}>{l`View`}</Link>} />
+        <Row title={l`Report a bug/Request a feature`} detail={<Link onClick={navigate.suggestions} className={S.caps}>{l`View`}</Link>} />
+        <Row title={l`Privacy Policy`} detail={<Link onClick={navigate.privacy} className={S.caps}>{l`View`}</Link>} />
+        <Row title={l`Terms of Service`} detail={<Link onClick={navigate.terms} className={S.caps}>{l`View`}</Link>} />
       </Table>
     </BasicCard>
   )

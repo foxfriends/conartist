057190ff7696529a0d8a3e48f78a28d4809053fc
Cameron Eldridge <cameldridge+git@gmail.com>
2019-01-29 22:33:25 -0500

    android: lets do this.

---
+++ android/app/src/main/java/com/cameldridge/conartist/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.cameldridge.conartist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sign_in)
+  }
+}

5707b5c5ac18bb32d435f475591f8f5139d29a75
Cameron Eldridge <cameldridge@gmail.com>
2025-03-22 20:15:39 -0400

    removing more seemingly unnecessary dependencies

--- Justfile
+++ Justfile
@@ -1,1 +1,9 @@
+default: serve
 
+[working-directory("server")]
+serve:
+    cargo run -- --any-origin
+
+[working-directory("web")]
+watch:
+    npm run watch
--- web/postcss.config.js
+++ web/postcss.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
-    "postcss-nesting": {},
-    "postcss-custom-properties": {},
+    // "postcss-nesting": {},
+    // "postcss-custom-properties": {},
   },
 };

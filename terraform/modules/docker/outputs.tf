2e134e9aa239b32546a7305b3dd7831df42128a7
Cameron Eldridge <cameldridge@gmail.com>
2025-04-23 22:06:45 -0400

    update outputs

--- terraform/modules/docker/outputs.tf
+++ terraform/modules/docker/outputs.tf
@@ -1,3 +1,11 @@
+output "container_port" {
+  value = local.internal_port
+}
+
 output "port" {
-  value = docker_container.conartist.ports[0].external
+  value = var.expose ? docker_container.conartist.ports[0].external : null
+}
+
+output "name" {
+  value = docker_container.conartist.name
 }
